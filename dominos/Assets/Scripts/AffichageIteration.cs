64eb582549d311d94c5be7d98bdbb3e13ab08a9c
Kmarouane <marouane-khatar@hotmail.com>
2016-01-07 13:26:47 +0100

    Ajout du script du HUD

---
+++ dominos/Assets/Scripts/AffichageIteration.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AffichageIteration : MonoBehaviour {
+
+	Text text;
+	public Iterations its;
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = "Itération : " + its.getIteration ();
+	}
+}
